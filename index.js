c3b09f92fc4d5a74e9dfba94389746f20e2f912a
MirajMalik <mirajmalik333@gmail.com>
2025-11-13 12:29:35 +0600

    redirect to the page using url shortened id/code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const{connectToMongoDB} = require("./connectionDb");
 const PORT = 8001;
+const URL = require('./models/url.model')
 const url = 'mongodb+srv://miraj_db:miraj_db@cluster0.xlenclv.mongodb.net/?appName=Cluster0'
 const urlRoute = require('./routes/url.route')
 
@@ -11,6 +12,25 @@ connectToMongoDB(url)
 app.use(express.json());
 
 app.use('/url',urlRoute);
+
+app.use('/:shortId',async (req,res)=>{
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+    {
+        shortId
+    },
+    { 
+      $push: {
+        visitHistory:{
+            timestamp: Date.now(),
+        },
+        },
+    }
+  );
+
+  res.redirect(entry.redirectUrl)
+
+});
 
 app.listen(PORT,()=>{
     console.log(`The server is running at port : ${PORT}`)
